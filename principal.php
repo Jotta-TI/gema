64ccae8508a3f5ac9d3d30829eb964a480d72d74
Jotta-TI <108303965+Jotta-TI@users.noreply.github.com>
2022-09-30 15:24:16 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -8,117 +8,121 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/principal.css">
 
+    <link href='assets/css1/core/main.min.css' rel='stylesheet' />
+        <link href='assets/css1/daygrid/main.min.css' rel='stylesheet' />
+        <script src='assets/js1/core/main.min.js'></script>
+        <script src='assets/js1/interaction/main.min.js'></script>
+        <script src='assets/js1/daygrid/main.min.js'></script>
+        <script src='assets/js1/core/locales/pt-br.js'></script>
+        <script>
 
-     <style>
+document.addEventListener('DOMContentLoaded', function () {
+                var calendarEl = document.getElementById('calendar');
 
-.modal{
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    locale: 'pt-br',
+                    plugins: ['interaction', 'dayGrid'],
+                    //defaultDate: '2019-04-12',
+                    editable: true,
+                    eventLimit: true,
+                    events: 'list_eventos.php',
+                    extraParams: function () {
+                        return {
+                            cachebuster: new Date().valueOf()
+                        };
+                    }
+                });
 
-        width: 300px;
-    }
+                calendar.render();
+            });
 
+        </script>
+        <style>
 
-.modal-content{
+            
 
-        width: 300px;
-        background-color: #9abf99;
+            #calendar {
+                max-width: 900px;
+                margin: 0 auto;
+                margin-top:100px;
+            }
 
-    }
+        </style>
 
-.list-group-item{
-
-         background-color: #9abf99;
-         
-    }
-
-.list-group-item:hover{
-
-background-color: #67A665;
-
-}
-
-.imagem {
-
-    width:100px ;
-    height:100px ;
-    border-radius:100px ;
-    margin-left: 75px;
-
-}
-
-.container-fluid{
-
-    background-color: #67A665;
-
-}
-
-.container-fluid h3{
-
-color: #fff;
-font-size:16px;
-
-}
-
-.container-fluid span{
-
-color: #b2d3b1;
-
-}
-
-#sidebar_btn {
-
-    
-    left:1px;
-    color: #fff;
-    font-size: 25px;
-    margin: 5px 0;
-    cursor: pointer;
-    transition:0.5s;
-    transition-property:color;
-    bottom: 8px;
-    
-}
-
-.sair_btn {
-
-text-decoration: none;
-color: #fff;
-
-}
-
-.sair_btn:hover{
-
-
-color: #fff;
-
-}
-
-
-
-</style>
 
 
 </head>
 
-<body style="background-image: url(assets/img/const.jpeg);">
+<body >
 
     <nav class="navbar navbar-light shadow" style="background-color: #67A665;">
         
         <div class="container-fluid">
 
+        
           <button class="navbar-toggler" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
             <ion-icon name="menu-outline" id="sidebar_btn"></ion-icon>
           </button>
-             <a class="navbar-brand">
-          <img src="assets/img/logoGEMA.jpg"
-          style="width:40px;"
-          class="rounded-pill">
-          </a>
-          <h3>G.E.M.A</h3>
+
+          
+         
+           <h3 style="margin-right:1140px;margin-top:08px; font-size:18px;">G.E.M.A</h3>
+          
+
+         <a class="navbar-brand">
+          <img src="assets/img/logoGEMA.jpg"style="width:40px;"class="rounded-pill">
+        </a>
+
+        <h2 style="margin-left:860px;margin-top:-48px; font-size:16px;text-transform: uppercase;" ><span>Gerenciador de Eventos Mediados Acadêmicos</span></h2>
+         
 
         </div>
 
       </nav>
+
+
+
+<div class="container-fluid">
+
+<div class="row">
+
+    <div class="col-sm-6 col-lg-4 mb-5" style="background-color:rgb(255, 255, 255);height:730px;width:330px;" >
+      
+    <div class="col" style="background-color:#67A665;margin-top:350px;height:340px;width:310px;">
+      Em construção
+    </div>
+
+    <div class="col" style="background-color:#67A665;margin-top:-650px;height:290px;width:310px;">
+    Em construção
+    </div>
+
+    </div>
+
+    <div class="col-xl-6 col-lg-4 mb-5" style="background-color:#fff">
+
+                
+    <div id='calendar' style="background-color: #fff;"></div> 
+
+      
+    </div>
+
+    <div class="col-sm-6 col-lg-4 mb-5" style="background-color:rgb(255, 255, 255);width:330px;">
+      
+    <div class="col" style="background-color:#67A665;height:655px;margin-top:35px;width:320px;">
+    Em construção
+    </div>
+
+    </div>
+
+</div>
+
+</div>
+  
+
+
+
 
   <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
