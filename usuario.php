69de30d1553f8277a30b4dd70efc41f965a856c5
Jotta-TI <108303965+Jotta-TI@users.noreply.github.com>
2022-09-30 17:25:35 -0300

    Update usuario.php

--- usuario.php
+++ usuario.php
@@ -33,7 +33,7 @@
             <div class="continue-button">
               <button><a href="index.php">Entrar</a></button>
             </div>
-              
+              <br>
             <div class="continue-button">
               <button type="submit">Enviar Código</button>
             </div>
@@ -47,4 +47,70 @@
       </body>
       
       </html>
+<?php
+function generatePassword($qtyCaraceters = 8){
+  //Letras minúsculas embaralhadas
+  $smallLetters = str_shuffle('abcdefghijklmnopqrstuvwxyz');
+  
+  //Letras maiúsculas embaralhadas
+  $capitalLetters = str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+  
+  //Números aleatórios
+  $numbers = (((date('Ymd') / 12) * 24) + mt_rand(800, 9999));
+  $numbers .= 1234567890;
+  
+  //Caracteres Especiais
+  $specialCharacters = str_shuffle('!@#$%*-');
+  
+  //Junta tudo
+  $characters = $capitalLetters.$smallLetters.$numbers.$specialCharacters;
+  
+  //Embaralha e pega apenas a quantidade de caracteres informada no parâmetro
+  $password = substr(str_shuffle($characters), 0, $qtyCaraceters);
+  
+  //Retorna a senha
+  return $password;
+}
+
+$cdg =  generatePassword();  
+$senha             = (isset($_POST['password']) )?$_POST["password"]: null;
+$nome             = (isset($_POST['firstname']) )?$_POST["firstname"]: null;
+$last            = (isset($_POST['lastname']) )?$_POST["lastname"]: null;
+$prontuario        = (isset($_POST['number']) )?$_POST["number"]: null;
+$corpo_academico   = (isset($_POST ['corpo']) )?$_POST["corpo"]: null;
+
+$emailp   = (isset($_POST ['email']) )?$_POST["email"]: null;
+
+$nCompleto = $nome." ".$last;
+$nome = $nCompleto;
+
+require 'email\Email.class.php';
+//include 'usuario.php';
+
+use email\Email;
+
+$email = new Email();
+$email->setFrom("empresajotati@gmail.com", "Empresa Jota TI");
+$email->addTo("$emailp", "$nome");
+$email->setSubject("Confirmação da Cadastro");
+$email->setMsgTxt("Seu código de confirmação é ".$cdg);
+
+$email->send_gmail();
+//$confirmar = "confim_cadastro.php";
+//header("Location: $confirmar ");
+//			die();
+
+
+$_SESSION['nome'] = $nome;
+
+$_SESSION['senha'] = $senha;
+
+$_SESSION['email'] = $email;
+
+$_SESSION['prontuario'] = $prontuario;
+
+$_SESSION['corpo_academico'] = $corpo_academico;
+
+$_SESSION['cdg'] = $cdg;
+?>
  
