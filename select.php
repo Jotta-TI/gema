23e259c0d1bd41ae65611d2f466ef86d55a7392c
Jotta-TI <108303965+Jotta-TI@users.noreply.github.com>
2022-10-06 13:07:36 -0300

    Update select.php

--- select.php
+++ select.php
@@ -5,7 +5,7 @@ $usuario             = (isset($_POST['usuario']) )?$_POST["usuario"]: null;
 echo $usuario;
 echo $senha;
 
-require_once "/home/hostdeprojetos/public_html/system/models/Select.class.php";
+require_once "/home/hostdeprojetos/public_html/gema/system/models/Select.class.php";
 
 $usuario =  new Select($usuario,$senha);
 $usuario->select();
