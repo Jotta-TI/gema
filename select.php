6d2255533f6f2085bd7eda3654aa8a7dbbe85066
Jotta-TI <108303965+Jotta-TI@users.noreply.github.com>
2022-10-06 02:30:02 -0300

    Update select.php

--- select.php
+++ select.php
@@ -3,7 +3,7 @@ $senha             = (isset($_POST['senha']) )?$_POST["senha"]: null;
 $usuario             = (isset($_POST['usuario']) )?$_POST["usuario"]: null;
 
 
-require_once "system\models\Select.class.php";
+require_once "/home/hostdeprojetos/public_html/system/models/Select.class.php";
 
 $usuario =  new Select($usuario,$senha);
 $usuario->select();
