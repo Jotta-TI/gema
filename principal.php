7d25638d4216ecb369bd731bce7eefd57d728bb5
Jotta-TI <108303965+Jotta-TI@users.noreply.github.com>
2022-10-13 14:11:41 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -135,7 +135,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <li class="list-group-item"> <ion-icon name="calendar-outline"></ion-icon> <span>Eventos no Calendário</span></li>
                 <li class="list-group-item"> <ion-icon name="settings-outline"></ion-icon><span>Processos</span></li>
                 <li class="list-group-item"><ion-icon name="chatbox-outline"></ion-icon><span>Notificações</span></li>
-                <li class="list-group-item"><ion-icon name="person-circle-outline"></ion-icon><span>Conta</span></li>
+                <li class="list-group-item"><ion-icon name="person-circle-outline"></ion-icon><span><a href="conta.php">Conta</a></span></li>
         </ul>
 
        
