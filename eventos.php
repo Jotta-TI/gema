258531ab706f63a2e27ad5e543f333c4df957d84
Jotta-TI <108303965+Jotta-TI@users.noreply.github.com>
2022-10-13 19:03:29 -0300

    Add files via upload

--- eventos.php
+++ eventos.php
@@ -6,7 +6,7 @@
 <meta charset='utf-8' />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="assets1/css/principal.css">
+<link rel="stylesheet" href="assets/css/principal.css">
 <link href='assets/css/lib/main.min.css' rel='stylesheet' />
 <script src='https://github.com/mozilla-comm/ical.js/releases/download/v1.4.0/ical.js'></script>
 <script src='assets/js/lib/main.min.js'></script>
